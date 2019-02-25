16f94519a827c3c7fa3efa48dc7518b35ea7266e
Katarzyn <kaatrix@gmail.com>
2019-02-25 22:09:31 +0100

    Add Article Fixtures, Add BaseFixtures, add heart incrementation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -59,12 +59,13 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
     {
-        // TODO - acctualy heart/unheart the article
+        $article->incrementHeartCount();
+        $em->flush();
 
         $logger->info('Article is being hearted');
 
-        return new JsonResponse(['hearts' => rand(5, 100)]);
+        return new JsonResponse(['hearts' => $article->getHeartCount()]);
     }
 }
