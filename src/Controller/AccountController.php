fd3ebb41038af51a58455db634f31f0a14064477
Katarzyn <kaatrix@gmail.com>
2019-03-18 19:38:56 +0100

    Add BaseController, make User account pretty

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,18 +5,20 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
 
 /**
  * @ISGranted("ROLE_USER")
  */
 
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     /**
      * @Route("/account", name="app_account")
      */
-    public function index()
+    public function index(LoggerInterface $logger)
     {
+        $logger->debug('Checking account page for '.$this->getUser()->getEmail());
         return $this->render('account/index.html.twig', [
             
         ]);
