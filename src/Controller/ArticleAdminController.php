874dbba10d4f29cc60016a9a7323852e0605a273
Katarzyn <kaatrix@gmail.com>
2019-03-24 13:10:50 +0100

    add Article Form, add new directory article list

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -8,6 +8,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ArticleFormType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ArticleRepository;
 
 class ArticleAdminController extends AbstractController
 {
@@ -15,15 +18,29 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
-        die('todo');
+        $form = $this->createForm(ArticleFormType::class);
 
-        return new Response(sprintf(
-            'New article id: #%d slug: %s',
-            $article->getId(),
-            $article->getSlug()
-        ));
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article created!');
+
+            return $this->redirectToRoute('admin_article_list');
+        }
+
+        return $this->render('article_admin/new.html.twig', [
+            'articleForm' => $form->createView()
+        ]);
     }
 
     /**
@@ -33,5 +50,16 @@ class ArticleAdminController extends AbstractController
     public function edit(Article $article)
     {
         dd($article);
+    }
+    /**
+     * @Route("/admin/article", name="admin_article_list")
+     */
+    public function list(ArticleRepository $articleRepo)
+    {
+        $articles = $articleRepo->findAll();
+
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles
+        ]);
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -130,6 +130,11 @@ class Article
         return $this;
     }
 
+    public function isPublished(): bool
+    {
+        return $this->publishedAt !== null;
+    }
+
     public function getHeartCount(): ?int
     {
         return $this->heartCount;
